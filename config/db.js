282b54ae22d74a3c6a5acf0bf3fd6e178289b427
marko-dev1 <villiermarko@gmail.com>
2025-08-06 21:10:39 +0100

    updated package.json

--- config/db.js
+++ config/db.js
@@ -1,10 +1,22 @@
 
+// const { Sequelize } = require('sequelize');
+
+// const sequelize = new Sequelize({
+//   dialect: 'sqlite',
+//   storage: './database.sqlite' 
+// });
+
+// module.exports = sequelize;
+
+
 const { Sequelize } = require('sequelize');
+const path = require('path');
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: './database.sqlite' 
+  storage: process.env.NODE_ENV === 'production' 
+    ? '/data/database.sqlite'  // Railway persistent storage path
+    : path.join(__dirname, 'database.sqlite'),
+  logging: console.log
 });
-
 module.exports = sequelize;
-
